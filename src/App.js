5a204dff3d18eb1f582feb237051d03ab3df9e96
marceortega <marcela.ortegao@mayor.cl>
2021-03-25 15:58:05 -0300

    commit Total

--- src/App.js
+++ src/App.js
@@ -1,25 +1,215 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+
+
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+
+import mapStyles from "./mapStyles";
+import { formatRelative } from "date-fns";
+import Tarjet from "../src/Tarjet.js";
+import bus from "../src/assets/bus.png";
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  height: "100vh",
+  width: "100vw",
+};
+
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+
+const center = {
+  lat: -33.5149429,
+  lng: -70.8961305,
+};
 
 function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyCpinY8TbKqlmzctEyo-l8f1Ezgc3cBUD4",
+    libraries,
+  });
+
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((event) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
+
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading...";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" container-fluid App">
+      <div className="row">
+      <div className="col">
+      <h1>
+        Good
+        <span>
+          <img
+            src={bus}
+            alt=""
+            style={{ width: "65px", height: "55px " }}
+          ></img>
+        </span>
+        Way{" "}
+      </h1>
+
+      <div className="col">
+      <Locate panTo={panTo} />
+
+      </div>
+      <div className="col">
+      <Search panTo={panTo} />
+      </div>
+      </div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: `/bus2.png`,
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+              scaledSize: new window.google.maps.Size(50, 50),
+            }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <Tarjet />
+              <p>{formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
+      </div> 
+      
     </div>
   );
 }
-
 export default App;
+function Locate({ panTo }) {
+  return (
+    <button
+      className="locate"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          () => null
+        );
+      }}
+    >
+      <img src="/compass.svg" alt="compass" />
+    </button>
+  );
+}
+
+function Search({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => -33.5149429, lng: () => -70.8961305 },
+      radius: 200 * 1000,
+    },
+  });
+  return (
+    <div className="search">
+      <Combobox
+        onSelect={async (address) => {
+          setValue(address, false);
+          clearSuggestions();
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng });
+          } catch (error) {
+            console.log("!error!");
+          }
+          //console.log(address);
+        }}
+      >
+        <ComboboxInput
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Ingrese Dirección"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
---
+++ src/Tarjet.js
@@ -0,0 +1,124 @@
+import React from "react";
+import school from "../src/assets/school.png";
+import icono from "../src/assets/icono.png";
+import iman from "../src/assets/iman.png";
+import walk from "../src/assets/walk.png";
+import drive from "../src/assets/drive.png";
+import transp from "../src/assets/transp.png";
+import { Icon, InlineIcon } from "@iconify/react";
+import heartFilled from "@iconify/icons-ant-design/heart-filled";
+import "./Tarjet.css";
+
+const Tarjet = () => {
+  return (
+    <div className=" container   tarjet">
+      <div class="row">
+        <div className="col-9  supName ">
+          <p className="pagraf mt-2">
+            Mauro-Sheridan Interdistrict Magnet for Science, Technology and
+            Communications
+          </p>
+        </div>
+        <div className="col-3  ">
+          <img className="supImage" src={school} alt=""></img>
+        </div>
+      </div>
+      <div class="row ">
+        <div className="col-3  pt-2  walk">
+          <span>
+            <img
+              className=""
+              style={{ width: "100%", height: "100% " }}
+              src={walk}
+              alt=""
+            ></img>{" "}
+          </span>
+          <span> 10min</span>
+        </div>
+        <div className="col-3 pt-2 drive">
+          <span>
+            <img
+              className=""
+              style={{ width: "50%", height: "50% " }}
+              src={drive}
+              alt=""
+            ></img>{" "}
+          </span>
+          <span> 5min</span>
+        </div>
+        <div className="col-3 pt-2 transp ">
+          <span>
+            <img
+              className=""
+              style={{ width: "50%", height: "50% " }}
+              src={transp}
+              alt=""
+            ></img>{" "}
+          </span>
+          <span> 7min</span>
+        </div>
+        <div className="col-3 pt-3 pl-4 circle  ">
+          <span>
+            <img
+              classname="circleTriColor"
+              style={{ width: "50%", height: "50%" }}
+              src={icono}
+              alt=""
+            ></img>{" "}
+          </span>
+        </div>
+      </div>
+
+      <div class="row  align-items-center">
+        <div className="col pt-2  d-flex justify-content-center centerImage">
+          <img
+            src={iman}
+            alt=""
+            style={{ width: "65px", height: "55px " }}
+          ></img>
+        </div>
+        <div className="col infoData">
+          <div class="row mt-2 pl-2  ">
+            <div classname="col  ">
+              <p className="grade">Grades Available:</p>
+            </div>
+          </div>
+          <div class="row pl-2 ">
+            <div classname="col">
+              <p className="detailsGrade"> PreK3 - 4</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row mt-2 footerTarjet">
+        <div className="col">
+          <button type="button" class="btn btn-outline-info btnInfo">
+            <a
+              className=" mt-1 d-flex justify-content-center textButton"
+              href="https://www.consiliumbots.com/"
+              target="_blank"
+            >
+              {" "}
+              EXPLORE SCHOOL
+            </a>
+          </button>
+        </div>
+        <div className="col">
+          <button
+            type="button"
+            class="btn btn-outline-danger d-flex justify-content-center likeButton"
+          >
+            <Icon
+              className=""
+              icon={heartFilled}
+              style={{ color: "#FC010A" }}
+            />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tarjet;
